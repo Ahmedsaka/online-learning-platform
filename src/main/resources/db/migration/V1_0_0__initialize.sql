1a437d5185575452c09fc8f1f747024625c2371e
ahmed.saka <ahmed.saka@interswitchgroup.com>
2020-07-01 22:03:48 +0100

    Initialized the database

---
+++ src/main/resources/db/migration/V1_0_0__initialize.sql
@@ -0,0 +1,10 @@
+INSERT INTO Role(name, description)
+VALUES('ROLE_STUDENT', 'Student');
+
+INSERT INTO Role(name, description)
+VALUES('ROLE_INSTRUCTOR', 'Instructor');
+
+INSERT INTO Role(name, description)
+VALUES('ROLE_ADMIN', 'Admin');
+
+
